c33924d219cdfffba654821e0af0bb0ddecabe2b
Connor Atkinson Williams <williamsca@uchicago.edu>
2017-11-30 21:34:38 -0600

    string testing

--- parser.c
+++ parser.c
@@ -4,5 +4,12 @@
 #include "parser.h"
 
 int main (int argc, char **argv) {
-  printf("%d\n", argc);
+  if (argc < 2) {
+    printf("Not enough arguments. Terminating.\n");
+    exit(-1);
+  }
+  char *sentence = argv[1];
+  char substring[100];
+  sprintf(substring, "%.4s", &sentence[10]);
+  puts(substring);
 }
